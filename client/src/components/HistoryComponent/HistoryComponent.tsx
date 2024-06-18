71ef2eea56737c710db2458f19f15c377268f75f
AlonaRahilevych <ragilevic@gmail.com>
2024-06-18 11:12:01 +0200

    HistoryComponent was created

--- client/src/components/HistoryComponent/HistoryComponent.tsx
+++ client/src/components/HistoryComponent/HistoryComponent.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import profile from '../../assets/images/profile.png';
 const HistoryComponent = () => {
   return (
-    <div className='history  '>
+    <div className='history flex flex-col items-center mx-2'>
       <div className='history__img rounded-full w-16 h-16 bg-gray-700 border-solid border-4 border-indigo-600'>
         <img
-          className=' rounded-full
-          w-full
-          h-full object-cover'
+          className='rounded-full w-full h-full object-cover'
           src={profile}
           alt=''
         />
       </div>
-      <div className='history__username'>username</div>
+      <div className='history__username text-sm mt-2'>username</div>
     </div>
   );
 };
