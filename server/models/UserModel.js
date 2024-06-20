f0a9a97f87ca19ebc49684a8dedf0ac37cb1b0a3
AlonaRahilevych <ragilevic@gmail.com>
2024-06-20 10:12:04 +0200

    userModel was modified by adding populate method

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
+const { Schema } = mongoose;
 const userSchema = mongoose.Schema({
   username: String,
   email: String,
   password: String,
   user_img: String,
   bio: String,
-  following: [String],
-  followers: [String],
-  savedPosts: [String],
-  posts: [String],
+  following: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+  followers: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+  saved_posts: [{ type: Schema.Types.ObjectId, ref: 'post' }],
+  posts: [{ type: Schema.Types.ObjectId, ref: 'post' }],
 });
 export const User = mongoose.model('user', userSchema);
