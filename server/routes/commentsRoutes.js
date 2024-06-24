c92742f91910bc4bba698bc95fb29f282d9ef347
AlonaRahilevych <ragilevic@gmail.com>
2024-06-24 15:21:12 +0200

    commentsRouter was reorganized

--- server/routes/commentsRoutes.js
+++ server/routes/commentsRoutes.js
@@ -1,80 +1,24 @@
 import express, { request, response } from 'express';
-import { User } from '../models/UserModel.js';
-import { Post } from '../models/PostModel.js';
-import { Comment } from '../models/CommentModel.js';
+import { User } from '../models/userModel.js';
+import { Post } from '../models/postModel.js';
+import { Comment } from '../models/commentModel.js';
+import {
+  addComment,
+  deleteCommentById,
+  getAllComments,
+  getCommentById,
+  updateCommentById,
+} from '../controllers/commentController.js';
+
 const commentsRouter = express.Router();
 
-commentsRouter.post('/', async (request, response) => {
-  try {
-    const newComment = {
-      user_id: request.body.user_id,
-      post_id: request.body.post_id,
-      text: request.body.text,
-      likes: request.body.likes,
-    };
-    const comment = await Comment.create(newComment);
-    return response.status(201).send(comment);
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
+commentsRouter.post('/', addComment);
 
-commentsRouter.get('/', async (request, response) => {
-  try {
-    const comments = await Comment.find({});
+commentsRouter.get('/', getAllComments);
 
-    return response.status(200).json({ data: comments });
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+commentsRouter.get('/:id', getCommentById);
 
-commentsRouter.get('/:id', async (request, response) => {
-  try {
-    const { id } = request.params;
-    console.log(`Fetching comment with id: ${id}`);
-    const comment = await Comment.findById(id);
-    if (!post) {
-      console.log(`Comment with id: ${id} not found`);
-      return response.status(404).json({ message: 'Post not found' });
-    }
-    console.log(`Comment with id: ${id} found`);
-    return response.status(200).json(post);
-  } catch (error) {
-    console.log(`Error fetching comment with id: ${id} - ${error.message}`);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-commentsRouter.put('/:id', async (request, response) => {
-  try {
-    const { id } = request.params;
-    const result = await Comment.findByIdAndUpdate(id, request.body);
-    if (!result) {
-      response.status(404).json({ message: 'Comment not find' });
-    }
-    return response
-      .status(200)
-      .send({ message: 'Cimment updated successfully' });
-  } catch (error) {
-    console.log(error.message);
-  }
-});
-commentsRouter.delete('/:id', async (request, response) => {
-  try {
-    const { id } = request.params;
-    const result = await Comment.findByIdAndDelete(id);
-    if (!result) {
-      response.status(404).json({ message: 'Comment not find' });
-    }
-    return response
-      .status(200)
-      .send({ message: 'Comment deleted successfully' });
-  } catch (error) {
-    console.log(error.massege);
-    response.status(500).send({ message: error.message });
-  }
-});
+commentsRouter.put('/:id', updateCommentById);
+commentsRouter.delete('/:id', deleteCommentById);
 
 export default commentsRouter;
