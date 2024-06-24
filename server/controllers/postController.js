99b1dd7903e76224975654aa13b89ad376be630d
AlonaRahilevych <ragilevic@gmail.com>
2024-06-24 15:20:56 +0200

    postsRouter was reorganized

---
+++ server/controllers/postController.js
@@ -0,0 +1,71 @@
+import { Post } from '../models/postModel';
+
+export const addPost = async (request, response) => {
+  try {
+    const newPost = {
+      user_id: request.body.user_id,
+      image_url: request.body.image_url,
+      caption: request.body.caption,
+      likes: request.body.likes,
+    };
+    const post = await Post.create(newPost);
+    return response.status(201).send(post);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+};
+
+export const getAllPosts = async (request, response) => {
+  try {
+    const posts = await Post.find({});
+
+    return response.status(200).json({ data: posts });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getPostById = async (request, response) => {
+  try {
+    const { id } = request.params;
+    console.log(`Fetching post with id: ${id}`);
+    const post = await Post.findById(id);
+    if (!post) {
+      console.log(`Post with id: ${id} not found`);
+      return response.status(404).json({ message: 'Post not found' });
+    }
+    console.log(`Post with id: ${id} found`);
+    return response.status(200).json(post);
+  } catch (error) {
+    console.log(`Error fetching user with id: ${id} - ${error.message}`);
+    response.status(500).send({ message: error.message });
+  }
+};
+
+export const updatePostById = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const result = await Post.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      response.status(404).json({ message: 'Post not find' });
+    }
+    return response.status(200).send({ message: 'Post updated successfully' });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deletePostById = async (request, response) => {
+  try {
+    const { id } = request.params;
+    const result = await Post.findByIdAndDelete(id);
+    if (!result) {
+      response.status(404).json({ message: 'Post not find' });
+    }
+    return response.status(200).send({ message: 'Post deleted successfully' });
+  } catch (error) {
+    console.log(error.massege);
+    response.status(500).send({ message: error.message });
+  }
+};
--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -1,75 +1,22 @@
 import express, { request, response } from 'express';
-import { User } from '../models/UserModel.js';
-import { Post } from '../models/PostModel.js';
+import { User } from '../models/userModel.js';
+import { Post } from '../models/postModel.js';
+import {
+  addPost,
+  deletePostById,
+  getAllPosts,
+  getPostById,
+  updatePostById,
+} from '../controllers/postController.js';
 const postsRouter = express.Router();
 
-postsRouter.post('/', async (request, response) => {
-  try {
-    const newPost = {
-      user_id: request.body.user_id,
-      image_url: request.body.image_url,
-      caption: request.body.caption,
-      likes: request.body.likes,
-    };
-    const post = await Post.create(newPost);
-    return response.status(201).send(post);
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
+postsRouter.post('/', addPost);
 
-postsRouter.get('/', async (request, response) => {
-  try {
-    const posts = await Post.find({});
+postsRouter.get('/', getAllPosts);
 
-    return response.status(200).json({ data: posts });
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+postsRouter.get('/:id', getPostById);
 
-postsRouter.get('/:id', async (request, response) => {
-  try {
-    const { id } = request.params;
-    console.log(`Fetching post with id: ${id}`);
-    const post = await Post.findById(id);
-    if (!post) {
-      console.log(`Post with id: ${id} not found`);
-      return response.status(404).json({ message: 'Post not found' });
-    }
-    console.log(`Post with id: ${id} found`);
-    return response.status(200).json(post);
-  } catch (error) {
-    console.log(`Error fetching user with id: ${id} - ${error.message}`);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-postsRouter.put('/:id', async (request, response) => {
-  try {
-    const { id } = request.params;
-    const result = await Post.findByIdAndUpdate(id, request.body);
-    if (!result) {
-      response.status(404).json({ message: 'Post not find' });
-    }
-    return response.status(200).send({ message: 'Post updated successfully' });
-  } catch (error) {
-    console.log(error.message);
-  }
-});
-postsRouter.delete('/:id', async (request, response) => {
-  try {
-    const { id } = request.params;
-    const result = await Post.findByIdAndDelete(id);
-    if (!result) {
-      response.status(404).json({ message: 'Post not find' });
-    }
-    return response.status(200).send({ message: 'Post deleted successfully' });
-  } catch (error) {
-    console.log(error.massege);
-    response.status(500).send({ message: error.message });
-  }
-});
+postsRouter.put('/:id', updatePostById);
+postsRouter.delete('/:id', deletePostById);
 
 export default postsRouter;
