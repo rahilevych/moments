1f1c3de97f5df725f6e2447bb73f8644f090269b
AlonaRahilevych <ragilevic@gmail.com>
2024-07-01 11:40:41 +0200

    multer was added

---
+++ server/middleware/multer.js
@@ -0,0 +1,13 @@
+import multer from 'multer';
+import path from 'path';
+export const multerUpload = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    let extension = path.extname(file.originalname);
+    if (extension !== '.jpg' && extension !== '.jpeg' && extension !== '.png') {
+      cb(new Error('File extension not supported'), false);
+      return;
+    }
+    cn(null, true);
+  },
+});
