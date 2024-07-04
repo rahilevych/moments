2818718457728591619093841e2f888b50900208
AlonaRahilevych <ragilevic@gmail.com>
2024-07-04 11:19:50 +0200

    UserModel was changed

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 const userSchema = mongoose.Schema({
-  username: { type: String, required: true },
-  email: String,
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  fullname: { type: String, required: true },
   password: { type: String, required: true },
-  user_img: String,
-  bio: String,
+  user_img: { type: String },
+  bio: { type: String },
   following: [{ type: Schema.Types.ObjectId, ref: 'user' }],
   followers: [{ type: Schema.Types.ObjectId, ref: 'user' }],
   saved_posts: [{ type: Schema.Types.ObjectId, ref: 'post' }],
   posts: [{ type: Schema.Types.ObjectId, ref: 'post' }],
+  createdAt: { type: Date, default: Date.now },
 });
 export const User = mongoose.model('user', userSchema);
 ``;
