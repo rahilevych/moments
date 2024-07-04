eabf9f1f5da40b691fba38577f1988e60e4e1c2d
AlonaRahilevych <ragilevic@gmail.com>
2024-07-04 11:24:25 +0200

    signup function was changed in auth context

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -4,13 +4,23 @@ import axios from 'axios';
 
 type AuthContextType = {
   user: UserType | null;
+  token: string;
+  email: string;
   username: string;
   password: string;
+  fullname: string;
+
   setUser: (user: UserType) => void;
   setUsername: (username: string) => void;
   setPassword: (password: string) => void;
+  setEmail: (username: string) => void;
+  setFullname: (password: string) => void;
+  setToken: (token: string) => void;
+
   signUp: () => Promise<void>;
   signIn: () => Promise<void>;
+  getUserProfile: () => Promise<void>;
+
   //email: string;
   //setEmail: (email: string) => void;
   // setSignUpPressed: (isPressed: boolean) => void;
@@ -25,9 +35,17 @@ type AuthContextType = {
 //define the initial value of context
 const initAuthContextValue = {
   user: {} as UserType,
+  token: '',
+
   username: '',
   password: '',
+  email: '',
+  fullname: '',
+
   setUser: () => {
+    throw new Error('context not initialised');
+  },
+  setToken: () => {
     throw new Error('context not initialised');
   },
   setPassword: () => {
@@ -37,8 +55,17 @@ const initAuthContextValue = {
   setUsername: () => {
     throw new Error('context not initialised');
   },
+  setEmail: () => {
+    throw new Error('context not initialised');
+  },
+
+  setFullname: () => {
+    throw new Error('context not initialised');
+  },
+
   signUp: () => Promise.resolve(),
   signIn: () => Promise.resolve(),
+  getUserProfile: () => Promise.resolve(),
 
   //   setEmail: () => {
   //     throw new Error('context not initialised');
@@ -67,34 +94,44 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextType>(initAuthContextValue);
 
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
-  let [user, setUser] = useState<UserType | null>(null);
+  const [email, setEmail] = useState('');
+  const [fullname, setFullname] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useState<UserType | null>(null);
+  const [token, setToken] = useState<string>('');
+
   console.log(password);
   console.log(username);
-
-  //   const [email, setEmail] = useState('');
-  //   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  //   const [loggedOut, setLoggedOut] = useState(false);
-  //   const [signUpPressed, setSignUpPressed] = useState(true);
-  //   const [loginPressed, setLoginPressed] = useState(false);
+  console.log(email);
+  console.log(fullname);
 
   const signUp = async () => {
     try {
       const response = await axios.post(
         'http://localhost:4003/users/registration',
         {
+          email,
           username,
           password,
+          fullname,
         }
       );
-      if (response.status === 200) {
+
+      if (response.status === 201) {
         const result = response.data;
-        console.log('Новый пользователь', result);
+        console.log('New user', result);
         setUser(result);
+      } else {
+        console.error('Unexpected response status:', response.status);
       }
     } catch (error) {
-      console.error('Ошибка при регистрации:', error);
+      if (axios.isAxiosError(error)) {
+        console.error('Axios error response data:', error.response?.data);
+        console.error('Axios error message:', error.message);
+      } else {
+        console.error('Error by signing up:', error);
+      }
     }
   };
 
@@ -107,12 +144,37 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
       if (response.status === 200) {
         const result = response.data;
         console.log('Signed in user', result);
+        setToken(result);
         if (result.token) {
           localStorage.setItem('token', result.token);
         }
       }
     } catch (error) {
       console.error('Error during signing in', error);
+    }
+  };
+
+  const getUserProfile = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.error('No token found in localStorage');
+      return;
+    }
+    try {
+      const response = await axios.get('http://localhost:4003/users/profile', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.status === 200) {
+        const user = response.data.user;
+        setUser(user);
+        console.log('User profile:', user);
+        const userId = user.id;
+        console.log('User id:', userId);
+      }
+    } catch (error) {
+      console.error('Error fetching user profile', error);
     }
   };
 
@@ -145,14 +207,24 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
   return (
     <AuthContext.Provider
       value={{
+        token,
         user,
+        email,
         username,
         password,
+        fullname,
+
         setUser,
         signUp,
         signIn,
+
+        setToken,
         setPassword,
         setUsername,
+        setFullname,
+        setEmail,
+
+        getUserProfile,
       }}>
       {children}
     </AuthContext.Provider>
