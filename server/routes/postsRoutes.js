3910641c59de12dd3f05360cb0c7afd57f66b6f3
AlonaRahilevych <ragilevic@gmail.com>
2024-07-05 19:52:24 +0200

    post routes were updated

--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -3,22 +3,25 @@ import { User } from '../models/userModel.js';
 import { Post } from '../models/postModel.js';
 import {
   addPost,
-  deletePostById,
-  getAllPosts,
-  getPostById,
-  updatePostById,
+  // deletePostById,
+  // getAllPosts,
+  // getPostById,
+  // updatePostById,
+  getUserPostsByUserId,
+  toggleLikePostById,
+  toggleSavePostById,
 } from '../controllers/postController.js';
 import { multerUpload } from '../middleware/multer.js';
+import JWTAuth from '../middleware/JWTAuth.js';
+
 const postsRouter = express.Router();
+postsRouter.post('/', multerUpload.single('image_url'), JWTAuth, addPost);
+postsRouter.get('/', JWTAuth, getUserPostsByUserId);
+postsRouter.post('/:id/like', JWTAuth, toggleLikePostById);
+postsRouter.post('/:id/save', JWTAuth, toggleSavePostById);
 
-postsRouter.post('/', multerUpload.single('image_url'), addPost);
-
-postsRouter.get('/', getAllPosts);
-
-postsRouter.get('/:id', getPostById);
-
-postsRouter.put('/:id', updatePostById);
-
-postsRouter.delete('/:id', deletePostById);
+// postsRouter.get('/:id', getPostById);
+// postsRouter.put('/:id', updatePostById);
+// postsRouter.delete('/:id', deletePostById);
 
 export default postsRouter;
