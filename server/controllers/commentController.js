536ba62a51f0f55f42eda5253bd23c7f153f1e6f
AlonaRahilevych <ragilevic@gmail.com>
2024-09-18 20:15:06 +0200

    in commentController were added simple changes like deleting logs and were changed some answers from server

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,42 +1,43 @@
 import { Comment } from '../models/commentModel.js';
+import { Post } from '../models/postModel.js';
 
 export const addComment = async (request, response) => {
   try {
     const newComment = {
-      user_id: request.body.user_id,
+      user_id: request.user._id,
       post_id: request.body.post_id,
       text: request.body.text,
       likes: request.body.likes,
     };
+
     const comment = await Comment.create(newComment);
-    return response.status(201).send(comment);
+    const postId = request.body.post_id;
+    await Post.findByIdAndUpdate(postId, { $push: { comments: comment._id } });
+    return response.status(201).json(comment);
   } catch (error) {
-    console.log(error.message);
+    console.error('Server error:', error.message);
     response.status(500).send({ message: error.message });
   }
 };
+
 export const getAllComments = async (request, response) => {
   try {
     const comments = await Comment.find({});
-
     return response.status(200).json({ data: comments });
   } catch (error) {
     console.log(error.message);
   }
 };
+
 export const getCommentById = async (request, response) => {
   try {
     const { id } = request.params;
-    console.log(`Fetching comment with id: ${id}`);
     const comment = await Comment.findById(id);
-    if (!post) {
-      console.log(`Comment with id: ${id} not found`);
+    if (!comment) {
       return response.status(404).json({ message: 'Post not found' });
     }
-    console.log(`Comment with id: ${id} found`);
-    return response.status(200).json(post);
+    return response.status(200).json(comment);
   } catch (error) {
-    console.log(`Error fetching comment with id: ${id} - ${error.message}`);
     response.status(500).send({ message: error.message });
   }
 };
@@ -45,11 +46,11 @@ export const updateCommentById = async (request, response) => {
     const { id } = request.params;
     const result = await Comment.findByIdAndUpdate(id, request.body);
     if (!result) {
-      response.status(404).json({ message: 'Comment not find' });
+      response.status(404).json({ message: 'Comment not found' });
     }
     return response
       .status(200)
-      .send({ message: 'Cimment updated successfully' });
+      .send({ message: 'Comment updated successfully' });
   } catch (error) {
     console.log(error.message);
   }
@@ -59,7 +60,7 @@ export const deleteCommentById = async (request, response) => {
     const { id } = request.params;
     const result = await Comment.findByIdAndDelete(id);
     if (!result) {
-      response.status(404).json({ message: 'Comment not find' });
+      response.status(404).json({ message: 'Error' });
     }
     return response
       .status(200)
