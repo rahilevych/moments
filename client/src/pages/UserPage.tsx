00df2f846df25773b415195984dd91207d344bc7
AlonaRahilevych <ragilevic@gmail.com>
2024-09-18 20:19:35 +0200

    User profile page was added on client

---
+++ client/src/pages/UserPage.tsx
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect } from 'react';
+import ProfileHeader from '../components/ProfileHeader';
+import UserPosts from '../components/UserPosts';
+import { PostContext } from '../context/PostContext';
+import { useParams } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
+import { AuthContext } from '../context/AuthContext';
+
+const UserPage = () => {
+  //const { posts, getPosts } = useContext(PostContext);
+  const { id } = useParams<{ id: string }>();
+  const { getUserById, setProfileUser } = useContext(UserContext);
+  const { getUserProfile, user } = useContext(AuthContext);
+  const { getUserPostsByUserId, getPosts } = useContext(PostContext);
+
+  const fetchUser = async () => {
+    if (id) {
+      const user = await getUserById(id);
+      console.log('user from userPage', user);
+      setProfileUser(user);
+      getUserPostsByUserId(user._id);
+      //getUserProfile();
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+
+    user && getUserPostsByUserId(user?._id);
+    //  }, [id, getUserById, setProfileUser]);
+  }, []);
+
+  // useEffect(() => {
+  //   getPosts();
+  // }, [getPosts]);
+
+  return (
+    <div className='profile-page flex flex-col items-center '>
+      <ProfileHeader />
+      <UserPosts />
+    </div>
+  );
+};
+
+export default UserPage;
