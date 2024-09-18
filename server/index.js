6babe89fbbd876c91941387cebac333273fdcad0
AlonaRahilevych <ragilevic@gmail.com>
2024-09-18 23:36:37 +0200

    some cors sitings were added

--- server/index.js
+++ server/index.js
@@ -14,7 +14,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://instclone-six.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 cloudinaryConfig();
 
 app.use(passport.initialize());
