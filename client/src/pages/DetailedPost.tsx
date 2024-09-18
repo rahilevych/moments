2e7b5b2af4a772b6f4db85b9f23fb4e99c8d3d4c
AlonaRahilevych <ragilevic@gmail.com>
2024-09-18 20:21:17 +0200

    detailed post page was changed

--- client/src/pages/DetailedPost.tsx
+++ client/src/pages/DetailedPost.tsx
@@ -1,103 +1,207 @@
-// import React, { useState, useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
-// import { Heart, ChatCircle, ShareFat, BookmarkSimple, Smiley } from '@phosphor-icons/react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  Heart,
+  ChatCircle,
+  ShareFat,
+  BookmarkSimple,
+  Smiley,
+} from '@phosphor-icons/react';
+import { PostContext } from '../context/PostContext';
+import { CommentContext } from '../context/CommentContext';
+import { AuthContext } from '../context/AuthContext';
+import { UserContext } from '../context/UserContext';
+import profile from '../assets/images/profile.png';
 
-// const PostDetailPage = () => {
-//   const { postId } = useParams();
-//   const [post, setPost] = useState(null);
-//   const [comments, setComments] = useState([]);
-//   const [newComment, setNewComment] = useState('');
+const DetailedPost = () => {
+  const { id } = useParams();
+  const {
+    posts,
+    toggleLikePost,
+    toggleSavePost,
+    setPost,
+    post,
+    getPostById,
+    getPosts,
+  } = useContext(PostContext);
+  const { user, getUserProfile } = useContext(AuthContext);
+  const { users } = useContext(UserContext);
+  const { getUserPostsByUserId } = useContext(PostContext);
 
-//   useEffect(() => {
-//     // Функция для получения данных поста
-//     const fetchPost = async () => {
-//       try {
-//         // Замените URL на ваш API для получения поста
-//         const response = await fetch(`/api/posts/${postId}`);
-//         const data = await response.json();
-//         setPost(data.post);
-//         setComments(data.comments);
-//       } catch (error) {
-//         console.error('Error fetching post:', error);
-//       }
-//     };
+  const {
+    setText,
+    addComment,
+    text,
+    getCommentsByIds,
+    comments,
+    setComment,
 
-//     fetchPost();
-//   }, [postId]);
+    setCurrentPost,
+  } = useContext(CommentContext);
 
-//   const handleNewCommentChange = (e) => {
-//     setNewComment(e.target.value);
-//   };
+  const findUsername = (userId: string) => {
+    const foundUser = users?.find((user) => user._id === userId);
+    return foundUser?.username;
+  };
+  const findAvatar = (userId: string) => {
+    const foundUser = users?.find((user) => user._id === userId);
+    return foundUser?.user_img || profile;
+  };
 
-//   const handlePostComment = async () => {
-//     try {
-//       // Логика отправки нового комментария
-//       const response = await fetch(`/api/posts/${postId}/comments`, {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({ text: newComment }),
-//       });
-//       const data = await response.json();
-//       setComments([...comments, data.comment]);
-//       setNewComment('');
-//     } catch (error) {
-//       console.error('Error posting comment:', error);
-//     }
-//   };
+  const handleLikeClick = async () => {
+    if (user && user._id && post) {
+      await toggleLikePost(post._id);
+    }
+  };
 
-//   if (!post) return <div>Loading...</div>;
+  const handleSaveClick = async () => {
+    if (user && post && post._id) {
+      console.log('save button was clicked');
+      await toggleSavePost(post._id, user._id);
+    }
+  };
 
-//   return (
-//     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-//       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-2xl">
-//         <div className="post__navigation flex flex-row px-4 py-2">
-//           <div className="post__user flex flex-row items-center">
-//             <div className="rounded-full ">
-//               <img src={post.user_img} alt="User" className="w-10 h-10 rounded-full" />
-//             </div>
-//             <p className="pl-4">{post.username}</p>
-//           </div>
-//           <div className="post__menu ml-auto"></div>
-//         </div>
-//         <div className="post__img w-full h-96">
-//           <img src={post.image_url} alt="Post" className="w-full h-full object-cover" />
-//         </div>
-//         <div className="post__reaction px-4 py-2 flex flex-row gap-2 justify-between">
-//           <div className="flex flex-row gap-2">
-//             <Heart size={24} />
-//             <ChatCircle size={24} />
-//             <ShareFat size={24} />
-//           </div>
-//           <BookmarkSimple size={24} />
-//         </div>
-//         {/* <div className="post__likes px-4">{post.likes.length} likes</div> */}
-//         <div className="post__description px-4 py-2">{post.caption}</div>
-//         <div className="post__comments px-4 py-2 text-sm text-gray-400">
-//           {comments.map((comment) => (
-//             <div key={comment._id} className="mb-2">
-//               <span className="font-semibold">{comment.username}</span> {comment.text}
-//             </div>
-//           ))}
-//         </div>
-//         <div className="px-4 text-xs text-gray-400">1 HOUR AGO</div>
-//         <div className="flex flex-row w-full relative">
-//           <input
-//             className="w-full my-2 pl-[50px] border-none"
-//             type="text"
-//             placeholder="Add a comment..."
-//             value={newComment}
-//             onChange={handleNewCommentChange}
-//           />
-//           <Smiley size={24} className="absolute left-3 top-4" />
-//           <button className="absolute right-3 top-4" onClick={handlePostComment}>
-//             Post
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+  const handleInputChangeComment = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
 
-// export default PostDetailPage;
+  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (user && post && text.trim() !== '' && id) {
+      const formData = new FormData();
+      formData.append('text', text);
+      formData.append('user_id', user._id);
+      formData.append('post_id', post._id);
+
+      await addComment(formData, id);
+      await getCommentsByIds(post.comments);
+      await getPostById(id);
+      setText('');
+    } else {
+      console.error('User, post or text is missing');
+    }
+  };
+
+  useEffect(() => {
+    getUserProfile();
+    console.log('user from det', user);
+    user && getUserPostsByUserId(user?._id);
+    console.log(id);
+    console.log(posts);
+    if (id && posts) {
+      getPostById(id);
+      const currentPost = posts.find((post) => post._id === String(id));
+      if (currentPost) {
+        setPost(currentPost);
+        if (currentPost.comments && currentPost.comments.length > 0) {
+          findAvatar(currentPost.user_id);
+          findUsername(currentPost.user_id);
+          getCommentsByIds(currentPost.comments);
+        }
+      } else {
+        console.error('Post not found');
+      }
+    }
+  }, []);
+  //}, [comments, addComment, getCommentsByIds]);
+
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-white'>
+      <div className='bg-white rounded-lg shadow-lg w-full max-w-5xl flex'>
+        <div className='w-2/3 p-4'>
+          <div className='post__navigation flex flex-row px-4 py-2'>
+            <div className='post__user flex flex-row items-center'>
+              <div className='rounded-full '>
+                <img
+                  alt='User'
+                  className='w-10 h-10 rounded-full'
+                  src={findAvatar(post?.user_id || profile)}
+                />
+              </div>
+              <p className='pl-4'>{findUsername(post?.user_id || '')}</p>
+            </div>
+            <div className='post__menu ml-auto'></div>
+          </div>
+          <div className='post__img w-full h-96'>
+            <img
+              alt='Post'
+              src={post?.image_url}
+              className='w-full h-full object-cover'
+            />
+          </div>
+          <div className='post__reaction px-4 py-2 flex flex-row gap-2 justify-between'>
+            <div className='flex flex-row gap-2'>
+              <Heart
+                size={24}
+                onClick={handleLikeClick}
+                style={{
+                  color:
+                    user && post?.likes.includes(user?._id) ? 'red' : 'gray',
+                }}
+              />
+              <ChatCircle size={24} />
+              <ShareFat size={24} />
+            </div>
+            <BookmarkSimple
+              size={24}
+              onClick={handleSaveClick}
+              style={{
+                color:
+                  post && user?.saved_posts?.includes(post?._id)
+                    ? 'red'
+                    : 'gray',
+              }}
+            />
+          </div>
+          <div className='post__description px-4 py-2'>
+            {post?.caption || 'No description available'}
+          </div>
+          <div className='px-4 text-xs text-gray-400'>1 HOUR AGO</div>
+          <div className='flex flex-row w-full relative mt-4'>
+            <form onSubmit={submitForm} className='flex w-full'>
+              <Smiley size={24} className='absolute left-3 top-2' />
+              <input
+                className='w-full pl-[50px] border rounded-full'
+                type='text'
+                placeholder='Add a comment...'
+                value={text}
+                onChange={handleInputChangeComment}
+              />
+              <button className='ml-2 px-4 py-2 bg-blue-500 text-white rounded-full'>
+                Post
+              </button>
+            </form>
+          </div>
+        </div>
+
+        <div
+          className='w-1/3 p-4 border-l overflow-y-auto'
+          style={{ maxHeight: '80vh' }}>
+          <h3 className='text-lg font-semibold mb-4'>Comments</h3>
+          {comments && comments.length > 0 ? (
+            comments.map((comment) => (
+              <div key={comment._id} className='mb-4'>
+                <div className='flex items-center mb-2'>
+                  <img
+                    alt='Avatar'
+                    className='w-8 h-8 rounded-full'
+                    src={findAvatar(comment.user_id || profile)}
+                  />
+                  <p className='ml-2 font-semibold'>
+                    {findUsername(comment.user_id || '')}
+                  </p>
+                </div>
+                <p>{comment.text}</p>
+              </div>
+            ))
+          ) : (
+            <p>No comments yet. Be the first to comment!</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailedPost;
