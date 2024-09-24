245b2c9ee7e956397ee4a3f40e45f0441adb8a99
AlonaRahilevych <ragilevic@gmail.com>
2024-09-24 14:07:00 +0200

    Improve DB models

--- server/models/PostModel.js
+++ server/models/PostModel.js
@@ -1,10 +1,10 @@
 import mongoose, { Schema } from 'mongoose';
 const postSchema = mongoose.Schema({
-  user_id: { type: Schema.Types.ObjectId, required: true },
+  user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   image_url: { type: String, required: true },
   caption: { type: String },
-  likes: { type: [Schema.Types.ObjectId], ref: 'user' },
-  comments: { type: [Schema.Types.ObjectId], ref: 'user' },
+  likes: { type: [Schema.Types.ObjectId], ref: 'User' },
+  comments: { type: [Schema.Types.ObjectId], ref: 'User' },
   createdAt: { type: Date, default: Date.now },
 });
-export const Post = mongoose.model('post', postSchema);
+export const Post = mongoose.model('Post', postSchema);
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -7,10 +7,10 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true },
   user_img: { type: String },
   bio: { type: String },
-  following: [{ type: Schema.Types.ObjectId, ref: 'user' }],
-  followers: [{ type: Schema.Types.ObjectId, ref: 'user' }],
-  saved_posts: [{ type: Schema.Types.ObjectId, ref: 'post' }],
+  following: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  followers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  saved_posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
   posts: [{ type: Schema.Types.ObjectId, ref: 'post' }],
   createdAt: { type: Date, default: Date.now },
 });
-export const User = mongoose.model('user', userSchema);
+export const User = mongoose.model('User', userSchema);
