f683868c829457c73806c01e4f5dd8433d50ef75
AlonaRahilevych <ragilevic@gmail.com>
2024-09-24 14:55:28 +0200

    Add server address check to determine base URL based on environment

--- client/src/utils/baseUrl.ts
+++ client/src/utils/baseUrl.ts
@@ -1,2 +1,6 @@
-export const baseUrl = import.meta.env.VITE_BASE_URL;
-//export const baseUrl = 'http://localhost:4003';
+const isLocal =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1';
+export const baseUrl = isLocal
+  ? import.meta.env.VITE_BASE_URL_LOCAL
+  : import.meta.env.VITE_BASE_URL;
