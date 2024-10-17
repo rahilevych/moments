d2fefba80fc3958f682a84e1f5f3da9ebc404c60
AlonaRahilevych <ragilevic@gmail.com>
2024-10-17 15:45:27 +0200

    Add search to SearchPage

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,22 +1,39 @@
-import React, { useContext, useEffect } from 'react';
-
+import React, { useContext, useEffect, useState } from 'react';
 import userAvatar from '../assets/images/profile.png';
-
 import { UserType } from '../types/UserType';
-import { Link } from 'react-router-dom'; // Импортируем Link из react-router-dom
+import { NavLink } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
-import { getAllUsers, toggleSubscribe } from '../services/userService';
+import {
+  fetchData,
+  getAllUsers,
+  toggleSubscribe,
+} from '../services/userService';
+import { PostContext } from '../context/PostContext';
 
 const SearchPage = () => {
-  const { users, user, setUsers } = useContext(UserContext);
+  const { users, user, setUsers, setUser } = useContext(UserContext);
+  const { setPosts } = useContext(PostContext);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(users);
 
-  const handleSearch = async (event: React.FormEvent) => {
-    event.preventDefault();
+  const handleSearch = (query: string) => {
+    if (!users) {
+      return;
+    }
+    const lowerCaseQuery = query.toLowerCase();
+    const results = users.filter(
+      (user) =>
+        user.username.toLowerCase().includes(lowerCaseQuery) ||
+        user.fullname.toLowerCase().includes(lowerCaseQuery)
+    );
+    setFilteredUsers(results);
   };
 
   const handleSubscribe = async (otherUserId: string) => {
     if (user) {
       await toggleSubscribe(otherUserId, user._id, setUsers);
+      await fetchData(setUser, setPosts);
+      await getAllUsers(setUsers);
     }
   };
 
@@ -27,35 +44,51 @@ const SearchPage = () => {
     );
   };
 
+  const fetchUsers = async () => {
+    try {
+      await fetchData(setUser, setPosts);
+      await getAllUsers(setUsers);
+    } catch (error) {}
+  };
+
   useEffect(() => {
-    getAllUsers(setUsers);
+    fetchUsers();
   }, []);
+
+  useEffect(() => {
+    setFilteredUsers(users);
+  }, [users]);
 
   return (
     <div className='p-4'>
       <h1 className='text-2xl font-bold mb-4'>Search Users</h1>
-      <form onSubmit={handleSearch} className='mb-4 flex space-x-2'>
+      <form className='mb-4 flex space-x-2'>
         <input
           type='text'
           placeholder='Search users...'
           className='p-2 border border-gray-300 rounded-md flex-grow'
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+            handleSearch(e.target.value);
+          }}
         />
-        <button type='submit' className='p-2 bg-blue-500 text-white rounded-md'>
-          Search
-        </button>
       </form>
-      <div className='space-y-4'>
-        {users?.map((followingUser) => (
+      <div className='space-y-4 '>
+        {filteredUsers?.map((followingUser) => (
           <div
             key={followingUser._id}
-            className='flex items-center space-x-4 p-4 border border-gray-200 rounded-md'>
-            <img
-              src={userAvatar}
-              alt={followingUser.username}
-              className='w-12 h-12 rounded-full'
-            />
-            <span className='font-medium'>{followingUser.username}</span>
-            <div className='ml-auto'>
+            className='flex justify-between items-center space-x-4 p-4 border border-gray-200 rounded-md'>
+            <div className='flex items-center gap-2'>
+              {' '}
+              <img
+                src={userAvatar}
+                alt={followingUser.username}
+                className='w-14 h-14 rounded-full'
+              />
+              <span className='font-medium'>{followingUser.username}</span>
+            </div>
+
+            <div className='ml-auto '>
               <button
                 onClick={() => handleSubscribe(followingUser._id)}
                 className={`px-4 py-2 rounded-md ${
@@ -66,11 +99,11 @@ const SearchPage = () => {
                 {isSubscribed(followingUser) ? 'Unsubscribe' : 'Subscribe'}
               </button>
 
-              <Link to={`/user/${followingUser._id}`}>
+              <NavLink to={`/user/${followingUser._id}`}>
                 <button className='px-4 py-2 ml-4 bg-blue-500 text-white rounded-md'>
                   View Profile
                 </button>
-              </Link>
+              </NavLink>
             </div>
           </div>
         ))}
