d4edc178183053c3debb78eb830ab5d794458c37
AlonaRahilevych <ragilevic@gmail.com>
2024-10-21 21:14:24 +0200

    update Nav component

--- client/src/components/NavComponent.tsx
+++ client/src/components/NavComponent.tsx
@@ -5,20 +5,20 @@ import {
   PaperPlaneTilt,
   PlusSquare,
 } from '@phosphor-icons/react';
-import { useContext, useEffect } from 'react';
+import { useContext, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import img from '../assets/images/profile.png';
-import { getUserProfile } from '../services/authService';
 import { PostContext } from '../context/PostContext';
 import { UserContext } from '../context/UserContext';
+import AddPost from '../components/AddPost';
+import Modal from '../components/Modal'; // Импортируем модальное окно
 import { getUserPostsByUserId } from '../services/postServices';
 
 const NavComponent = () => {
-  const { user, setUser, setProfileUser, profileUser } =
-    useContext(UserContext);
+  const { user, setProfileUser, profileUser } = useContext(UserContext);
   const { post, setPosts } = useContext(PostContext);
 
   const navigate = useNavigate();
+  const [isAddPostModalOpen, setIsAddPostModalOpen] = useState(false);
 
   const handleNavigateToProfile = () => {
     if (user && user._id) {
@@ -28,13 +28,13 @@ const NavComponent = () => {
     }
   };
 
-  useEffect(() => {
-    getUserProfile(setUser);
-  }, []);
+  const toggleAddPostModal = () => {
+    setIsAddPostModalOpen(!isAddPostModalOpen);
+  };
+
   return (
     <nav className='flex flex-col items-start p-4 space-y-6'>
       <NavLink to={'home'}>
-        {' '}
         <div className='flex items-center space-x-2 hover:bg-gray-100 p-2 rounded-lg cursor-pointer'>
           <House size={32} />
           <p>Home</p>
@@ -58,12 +58,18 @@ const NavComponent = () => {
         <p>Notifications</p>
       </div>
 
-      <NavLink to={'add-post'}>
-        <div className='flex items-center space-x-2 hover:bg-gray-100 p-2 rounded-lg cursor-pointer'>
-          <PlusSquare size={32} />
-          <p>Create</p>
-        </div>
-      </NavLink>
+      {/* Кнопка для создания поста */}
+      <div
+        className='flex items-center space-x-2 hover:bg-gray-100 p-2 rounded-lg cursor-pointer'
+        onClick={toggleAddPostModal}>
+        <PlusSquare size={32} />
+        <p>Create</p>
+      </div>
+
+      {/* Модальное окно для создания поста */}
+      <Modal isOpen={isAddPostModalOpen} onClose={toggleAddPostModal}>
+        <AddPost />
+      </Modal>
 
       <div
         className='flex items-center space-x-2 hover:bg-gray-100 p-2 rounded-lg cursor-pointer'
