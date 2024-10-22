3f96991f72714f1495913fbbc3f620e71ce83e91
AlonaRahilevych <ragilevic@gmail.com>
2024-10-22 21:18:24 +0200

    Update Add post modal window

--- client/src/components/AddPost.tsx
+++ client/src/components/AddPost.tsx
@@ -3,24 +3,31 @@ import { PostContext } from '../context/PostContext';
 import { UserContext } from '../context/UserContext';
 import { addPost } from '../services/postServices';
 import { CommentContext } from '../context/CommentContext';
+import { CaretLeft, Images } from '@phosphor-icons/react';
 
 const AddPost = () => {
   const { setCaption, caption, setPost, setPosts } = useContext(PostContext);
   const { setComments } = useContext(CommentContext);
   const { user } = useContext(UserContext);
   const selectedFile = useRef<File | null>(null);
-
-  // Состояние для отслеживания успешного добавления поста
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
   const [isPostAdded, setIsPostAdded] = useState(false);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       selectedFile.current = e.target.files[0];
+      const fileURL = URL.createObjectURL(selectedFile.current);
+      setImagePreview(fileURL);
     }
   };
 
   const handleInputChangeCaption = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCaption(e.target.value);
+  };
+  const handleBackToUpload = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setImagePreview(null);
   };
 
   const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -30,68 +37,92 @@ const AddPost = () => {
       formData.append('image_url', selectedFile.current);
       formData.append('caption', caption);
       formData.append('user_id', user?._id);
-
       await addPost(formData, setPost, setPosts, setComments);
-
-      // Устанавливаем состояние после успешного добавления
       setIsPostAdded(true);
-
-      // Сброс формы после добавления поста
+      setImagePreview(null);
       selectedFile.current = null;
       setCaption('');
     }
   };
 
   return (
-    <div className='h-auto flex items-center justify-center bg-white'>
-      <div className='bg-white p-8 rounded-lg shadow-lg w-full max-w-lg'>
-        <h2 className='text-3xl font-bold text-center mb-6 text-gray-800'>
-          Add a New Post
-        </h2>
+    <>
+      {!imagePreview ? (
+        <div className='flex flex-col items-center justify-center bg-white w-full h-full p-4'>
+          <h2 className='font-bold text-center text-gray-600 border-b-2 border-gray-300 w-full py-2'>
+            Create new post
+          </h2>
+          <div className='bg-white p-2 w-full h-full flex flex-col items-center justify-between '>
+            <div className='p-10'>
+              <label
+                className='block text-sm font-medium text-gray-700 mb-2'
+                htmlFor='image'>
+                Drag photos here
+              </label>
+              <Images size={128} />
+              <input
+                onChange={handleFileChange}
+                type='file'
+                id='image'
+                accept='image/*'
+                className='hidden'
+              />
+            </div>
+            <button
+              type='button'
+              onClick={() => document.getElementById('image')?.click()}
+              className='w-full py-3 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75 transition duration-300'>
+              Upload Photo
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className='flex flex-col items-center justify-center bg-white w-full p-4'>
+          <div className='flex items-center justify-between border-b-2 border-gray-300 w-full py-2'>
+            <button onClick={handleBackToUpload}>
+              <CaretLeft
+                size={32}
+                className='font-bold text-center text-gray-600 cursor-pointer'
+              />
+            </button>
+            <h2 className='font-bold text-center text-gray-600 '>
+              Create new post
+            </h2>
+          </div>
 
-        {/* Оповещение об успешном добавлении поста */}
-        {isPostAdded && (
-          <div className='mb-6 text-green-600 font-semibold text-center'>
-            Post added successfully!
+          <div className='bg-white p-2 w-full flex flex-col md:flex-row'>
+            <div className='flex-1'>
+              <img
+                src={imagePreview}
+                alt='Preview'
+                className='w-full h-96 rounded-md'
+              />
+              <form onSubmit={submitForm} method='post'>
+                <div className='mb-6 pt-4'>
+                  <label
+                    className='block text-sm font-medium text-gray-700 mb-2'
+                    htmlFor='caption'>
+                    Caption
+                  </label>
+                  <input
+                    onChange={handleInputChangeCaption}
+                    value={caption}
+                    id='caption'
+                    className='block w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
+                    placeholder='Write a caption...'
+                  />
+                </div>
+                <button
+                  type='submit'
+                  className='w-full py-3 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75 transition duration-300'>
+                  Add Post
+                </button>
+              </form>
+            </div>
           </div>
-        )}
-
-        <form onSubmit={submitForm} method='post'>
-          <div className='mb-6'>
-            <label
-              className='block text-sm font-medium text-gray-700 mb-2'
-              htmlFor='image'>
-              Upload Image
-            </label>
-            <input
-              onChange={handleFileChange}
-              type='file'
-              id='image'
-              accept='image/*'
-              className='block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100'
-            />
-          </div>
-          <div className='mb-6'>
-            <label
-              className='block text-sm font-medium text-gray-700 mb-2'
-              htmlFor='caption'>
-              Caption
-            </label>
-            <input
-              onChange={handleInputChangeCaption}
-              value={caption}
-              id='caption'
-              className='block w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
-              placeholder='Write a caption...'></input>
-          </div>
-          <button
-            type='submit'
-            className='w-full py-3 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75 transition duration-300'>
-            Post
-          </button>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
