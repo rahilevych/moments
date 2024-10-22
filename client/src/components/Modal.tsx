bdf8e4a0bd093a556ffe45d080ad4224792ad7d1
AlonaRahilevych <ragilevic@gmail.com>
2024-10-22 21:16:05 +0200

    Update modal styles

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,3 +1,4 @@
+import { Trash, X } from '@phosphor-icons/react';
 import React from 'react';
 
 interface ModalProps {
@@ -10,13 +11,13 @@ const Modal = (props: ModalProps) => {
   if (!props.isOpen) return null;
 
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50'>
-      <div className='bg-white p-8 rounded-lg shadow-lg max-w-lg relative overflow-auto max-h-[50vh]'>
-        <button
-          onClick={props.onClose}
-          className='text-red-500 absolute top-2 right-2 text-2xl'>
-          X
-        </button>
+    <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 mt-0  '>
+      <button
+        onClick={props.onClose}
+        className='text-white-500 absolute top-2 right-2 text-2xl cursor-pointer'>
+        <X size={32} />
+      </button>
+      <div className='bg-white  rounded-lg shadow-lg relative overflow-auto w-auto max-w-[80vh]  max-h-[80vh] min-w-96 min-h-96'>
         {props.children}
       </div>
     </div>
