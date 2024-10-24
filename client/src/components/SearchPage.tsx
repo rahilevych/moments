8411ef748f0b3c6b73bf7eb6ed46a1c1562fd7cd
AlonaRahilevych <ragilevic@gmail.com>
2024-10-24 11:53:10 +0200

    Update search page and show it as modal window

--- client/src/components/SearchPage.tsx
+++ client/src/components/SearchPage.tsx
@@ -1,24 +1,22 @@
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../context/UserContext';
-import { fetchData, getAllUsers } from '../services/userService';
-import { PostContext } from '../context/PostContext';
-import { Search } from '../components/Search';
-import UsersList from '../components/UsersList';
+import { getAllUsers } from '../services/userService';
+import { Search } from './Search';
+import UsersList from './UsersList';
 
 const SearchPage = () => {
-  const { users, setUsers, setUser } = useContext(UserContext);
-  const { setPosts } = useContext(PostContext);
+  const { users, setUsers, user } = useContext(UserContext);
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredUsers, setFilteredUsers] = useState(users);
 
   const fetchUsers = async () => {
     try {
-      await fetchData(setUser, setPosts);
-      await getAllUsers(setUsers);
+      setUsers(await getAllUsers());
     } catch (error) {}
   };
 
   useEffect(() => {
+    console.log('user search page>>>', user);
     fetchUsers();
   }, []);
 
