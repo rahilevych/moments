7033e0a85cbbd72997ef99bc4061a877808ff172
AlonaRahilevych <ragilevic@gmail.com>
2025-02-04 12:21:49 +0100

    refactor:delete unused code

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 import { getUserProfile, signIn } from '../services/authService';
 import { getUserById } from '../services/userService';
