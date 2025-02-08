f06d15e721da66a8584769047b6912c22b3d61b9
AlonaRahilevych <ragilevic@gmail.com>
2025-02-08 20:04:23 +0100

    chore: update cors headers

--- server/index.js
+++ server/index.js
@@ -21,7 +21,19 @@ const addMiddlewares = () => {
       credentials: true,
     })
   );
-
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://instclone-client.vercel.app'
+    );
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   cloudinaryConfig();
