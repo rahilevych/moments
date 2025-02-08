df9fc8d063cdc375fba4f9d156cda49aea943979
AlonaRahilevych <ragilevic@gmail.com>
2025-02-08 20:33:27 +0100

    chore:update dependencies

--- client/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // укажите путь к вашим файлам
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms')],
-};
