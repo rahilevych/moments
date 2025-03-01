c58ea0f675dbe2f36dfc4a18302dd2fbae1d460b
AlonaRahilevych <ragilevic@gmail.com>
2025-03-01 13:55:54 +0100

    refactor: remove Followings and Followers components and create a unified  list component to avoid code duplication

---
+++ client/src/components/ConnectionsList.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import UsersList from './UsersList';
+import { Search } from './Search';
+import { UserType } from '../types/UserType';
+interface Props {
+  profileUser: UserType;
+  onClose: () => void;
+  modalType: string;
+}
+const ConnectionsList: React.FC<Props> = ({
+  onClose,
+  profileUser,
+  modalType,
+}) => {
+  const [users, setUsers] = useState<UserType[] | null>(null);
+  const [, setSearchQuery] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(users);
+  const fetchUsers = async (field: string, profileUser: UserType) => {
+    try {
+      if (profileUser) {
+        const usersList = profileUser[field as keyof UserType];
+        if (Array.isArray(usersList)) {
+          setUsers(usersList as UserType[]);
+        } else {
+          console.error(`Expected an array but got:`, usersList);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchUsers(modalType, profileUser);
+    console.log('users>>>>');
+  }, []);
+
+  useEffect(() => {
+    setFilteredUsers(users);
+  }, [users]);
+
+  return (
+    <div className='p-4 max-h-[calc(100vh-150px)] overflow-y-auto'>
+      {users && (
+        <Search
+          setFilteredUsers={setFilteredUsers}
+          setSearchQuery={setSearchQuery}
+          title={'Search followers'}
+          users={users}
+        />
+      )}
+      <UsersList filteredUsers={filteredUsers} onClose={onClose} />
+    </div>
+  );
+};
+export default ConnectionsList;
