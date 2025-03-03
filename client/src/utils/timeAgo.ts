acbc4b08d8b2804900440cc3cb21744de32c6ac6
AlonaRahilevych <ragilevic@gmail.com>
2025-03-03 22:41:03 +0100

    feat: rewrite function to show short time

--- client/src/utils/timeAgo.ts
+++ client/src/utils/timeAgo.ts
@@ -1,18 +1,44 @@
-export const timeAgo = (createdAt: string | Date) => {
+export const timeAgo = (
+  createdAt: string | Date,
+  options: { format?: 'long' | 'short' } = {}
+) => {
+  const { format = 'long' } = options;
   const createdDate = new Date(createdAt);
   const now = new Date();
   const seconds = Math.floor((now.getTime() - createdDate.getTime()) / 1000);
+  let interval: number;
 
-  let interval = Math.floor(seconds / 31536000);
-  if (interval >= 1) return `${interval} YEAR${interval === 1 ? '' : 'S'} AGO`;
-  interval = Math.floor(seconds / 2592000);
-  if (interval >= 1) return `${interval} MONTH${interval === 1 ? '' : 'S'} AGO`;
-  interval = Math.floor(seconds / 86400);
-  if (interval >= 1) return `${interval} DAY${interval === 1 ? '' : 'S'} AGO`;
-  interval = Math.floor(seconds / 3600);
-  if (interval >= 1) return `${interval} HOUR${interval === 1 ? '' : 'S'} AGO`;
-  interval = Math.floor(seconds / 60);
-  if (interval >= 1)
-    return `${interval} MINUTE${interval === 1 ? '' : 'S'} AGO`;
-  return `${seconds} SECOND${seconds === 1 ? '' : 'S'} AGO`;
+  if (seconds >= 31536000) {
+    interval = Math.floor(seconds / 31536000);
+    return format === 'short'
+      ? `${interval}y`
+      : `${interval} YEAR${interval === 1 ? '' : 'S'} AGO`;
+  }
+  if (seconds >= 2592000) {
+    interval = Math.floor(seconds / 2592000);
+    return format === 'short'
+      ? `${interval}mo`
+      : `${interval} MONTH${interval === 1 ? '' : 'S'} AGO`;
+  }
+  if (seconds >= 86400) {
+    interval = Math.floor(seconds / 86400);
+    return format === 'short'
+      ? `${interval}d`
+      : `${interval} DAY${interval === 1 ? '' : 'S'} AGO`;
+  }
+  if (seconds >= 3600) {
+    interval = Math.floor(seconds / 3600);
+    return format === 'short'
+      ? `${interval}h`
+      : `${interval} HOUR${interval === 1 ? '' : 'S'} AGO`;
+  }
+  if (seconds >= 60) {
+    interval = Math.floor(seconds / 60);
+    return format === 'short'
+      ? `${interval}m`
+      : `${interval} MINUTE${interval === 1 ? '' : 'S'} AGO`;
+  }
+  return format === 'short'
+    ? `${seconds}s`
+    : `${seconds} SECOND${seconds === 1 ? '' : 'S'} AGO`;
 };
